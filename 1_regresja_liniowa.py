30cd3d538f729a7c81328c548dc3815b9bbc668a
student-wroclaw <student-wroclaw@EDU-WSB.local>
2026-01-10 10:41:41 +0100

    1

--- 1_regresja_liniowa.py
+++ 1_regresja_liniowa.py
@@ -4,5 +4,18 @@ import pandas as pd
 # time.sleep(2)
 print('Program')
 
-df = pd.read_csv('weight-height.csv',sep=';')       # czytanie pliku csv
+df = pd.read_csv('weight-height.csv', sep=';')       # czytanie pliku csv
 print(df)
+# print(type(df))
+
+# df.Height = df.Height * 2.54
+df.Height *= 2.54
+# df.Weight = df.Weight / 2.2
+df.Weight /= 2.2
+
+print('\nPo zmianie jednostek')
+print(df)
+print(f'ksztalt danych {df.shape}')
+print('\nDescribe')
+print(df.describe().T.round(2).to_string())
+
